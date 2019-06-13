e888056833e1d15df8446902f537496d507347d3
mauricio <mauhiga@unicamp.br>
2019-06-13 17:20:29 -0300

    Primeira versao

---
+++ blog/models/comment.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
